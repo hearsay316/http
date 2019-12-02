74e9399146ddf7de31e8cd930478b804983612eb
hearsay316 <hearsay316@gmail.com>
2019-12-02 16:44:45 +0800

    增加了  首页设置

--- static-server-async.js
+++ static-server-async.js
@@ -9,31 +9,32 @@ const { createReadStream, readFileSync, constants } = require("fs");
 const ejs = require("ejs");
 const template = readFileSync(path.resolve(__dirname, "template.html"), "utf8");
 class Server {
-  constructor({ port, cwd ,index}) {
+  constructor({ port, cwd, index }) {
     this.port = port || 3000;
     this.cwd = cwd || process.cwd();
     this.template = template;
-    this.index = index|| "index.html";
+    this.index = index || "index.html";
   }
   async handleRequest(req, res) {
     let { pathname } = url.parse(req.url);
     pathname = decodeURIComponent(pathname);
     let filepath = path.join(this.cwd, pathname);
+    console.log(filepath)
     try {
       let statObj = await fs.stat(filepath);
       if (statObj.isDirectory()) {
         let filepathIndex = path.join(filepath, this.index);
         await fs.access(filepathIndex, constants.F_OK);
-        console.log(filepath, filepathIndex);
         this.sendFile(req, res, filepathIndex);
       } else {
+        console.log(filepath,66666);
         this.sendFile(req, res, filepath);
       }
       // let content = await fs.readFile(filepath, "utf8");
       // res.end(content);
       // 也可以用流
     } catch (e) {
-      console.log(e);
+       console.log(e);
       if (e.path.includes(this.index)) {
         let dist = await fs.readdir(filepath);
         let str = ejs.render(this.template, {
@@ -48,12 +49,12 @@ class Server {
     }
   }
   sendFile(req, res, filepath) {
-    console.log(filepath, "属性", mime.getType(filepath));
+  //  console.log(filepath, "属性", mime.getType(filepath));
     res.setHeader("Content-Type", mime.getType(filepath) + ";charset=utf-8");
     createReadStream(filepath).pipe(res);
   }
   sendError(req, res, e) {
-    console.log(e, "555");
+  //  console.log(e, "555");
     res.statusCode = 404;
     res.end("Not found");
   }
@@ -66,13 +67,13 @@ class Server {
     Hit CTRL-C to stop the server
     `);
     });
-    server.on("error",(err)=>{
-     // console.log(err,66666666)
-      if(err.code==="EADDRINUSE"){
+    server.on("error", err => {
+      // console.log(err,66666666)
+      if (err.code === "EADDRINUSE") {
         ++this.port;
-        this.start()
+        this.start();
       }
-    })
+    });
   }
 }
 module.exports = Server;
